d5bc9492eff6559fd87762d6ea17b4428c504686
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-12-15 12:57:26 -0600

    chore: setup next

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  swcMinify: true,
+  experimental: {
+    concurrentFeatures: true,
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.(woff2|png|jpg|mp4|glb)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.mdx$/,
+        use: ['next-swc-loader', 'mdx-loader'],
+      }
+    );
+
+    return config;
+  },
+};
