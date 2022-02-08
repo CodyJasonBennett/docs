78777768ae2bfffc364dece9784e9000c8b4699b
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-02-07 22:45:55 -0600

    chore: cleanup grid styles

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -4,6 +4,16 @@ import Preview from 'components/Preview'
 const components = {
   iframe: Preview,
   Preview,
+  ul: (props) => (
+    <p>
+      <ul {...props} />
+    </p>
+  ),
+  blockquote: (props) => (
+    <p>
+      <blockquote {...props} />
+    </p>
+  ),
 }
 
 export default function Post({ children }) {
