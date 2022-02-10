7972d2a052c2f9bcc2f1ca5185f8afa91d952fa5
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-02-10 02:08:54 -0600

    chore: preload fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import DocsProvider from 'components/DocsProvider'
 import 'styles/index.css'
 import 'styles/reset.css'
@@ -5,6 +6,11 @@ import 'styles/reset.css'
 export default function App({ Component, pageProps }) {
   return (
     <DocsProvider>
+      <Head>
+        <link rel="preload" href="/fonts/inter-var.woff2" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/inter-var-italic.woff2" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/roboto-mono.woff2" as="font" crossOrigin="" />
+      </Head>
       <main>
         <Component {...pageProps} />
       </main>
